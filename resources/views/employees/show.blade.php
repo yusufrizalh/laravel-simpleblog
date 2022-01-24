f5948f992abeec1f6d329d95b5defb506c702e3a
Yusuf Rizal <yusufrizalh@gmail.com>
2022-01-24 14:23:05 +0700

    part 1 - routing view subfolder

---
+++ resources/views/employees/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Employees Show</title>
+</head>
+
+<body>
+    <h1>Show page from employees > show.blade.php</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 Route::view('/contact', 'contact');
 Route::view('/gallery', 'gallery');
+
+// Mengakses file views dari sub-folder: employees
+Route::view('employees/index', 'employees/index');
+Route::view('employees/create', 'employees/create');
+Route::view('employees/show', 'employees/show');
+Route::view('employees/edit', 'employees/edit');
+Route::view('employees/delete', 'employees/delete');
+
+// Mengakses file views dari sub-folder: series
