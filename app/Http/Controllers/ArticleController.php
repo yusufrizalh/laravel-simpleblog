d5822057ec8426ee12fa180c7f64c59b9d912db6
Yusuf Rizal <yusufrizalh@gmail.com>
2022-01-25 12:42:34 +0700

    part 3 - make:model

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,7 +8,12 @@ class ArticleController extends Controller
 {
     public function show($slug){
         // return $slug;
-        $article = \DB::table('articles')->where('slug', $slug)->first();
+        // $article = \DB::table('articles')->where('slug', $slug)->first();
+        $article = \App\Article::where('slug', $slug)->first();
+        // jika data tidak ditemukan maka arahkan ke halaman 404 not found
+        if (is_null($article)) {
+            abort(404);
+        }
         // dd($article);
         return view('articles/show', compact('article'));
     }
