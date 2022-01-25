53fc567ce8708adc9749700c07b74172c41c8cb2
Yusuf Rizal <yusufrizalh@gmail.com>
2022-01-25 12:55:28 +0700

    part 3 - model binding

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,19 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function show($slug){
-        // return $slug;
-        // $article = \DB::table('articles')->where('slug', $slug)->first();
-        $article = \App\Article::where('slug', $slug)->first();
-        // jika data tidak ditemukan maka arahkan ke halaman 404 not found
-        if (is_null($article)) {
-            abort(404);
-        }
-        // dd($article);
+    public function show(Article $article){
         return view('articles/show', compact('article'));
     }
 }
